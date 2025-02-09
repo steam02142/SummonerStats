a7f146c3a12e2b1523012b8d8f5b2815e99c9f7e
spencer muise <spencermuise@shaw.ca>
2025-02-09 11:56:29 -0800

    Increased readability on homescreen + Repositioned header and input field - Extra guiding text

--- src/Home.jsx
+++ src/Home.jsx
@@ -2,22 +2,10 @@ import InputForm from './components/input'
 
 function Home() {
     return (
-        <div className="min-h-screen w-full flex justify-center bg-cover bg-[url('/garen-background.webp')]">
-            <div className="content max-w-4xl">
-                <h1 className="text-5xl mt-4 font-bold text-gray-200 text-center">SummonerStats</h1>
-                <div className="mt-44 mb-14 text-[#f1f1f1] align-middle">
-                    <span className="block text-4xl font-bold text-[#f1f1f1]">
-                        Welcome
-                    </span>
-                    <span className="font-medium text-lg">Please enter your </span>
-                    <span className="font-medium text-lg bg-blue-600 rounded-md">username</span>
-                    <span className="font-medium text-lg"> and </span>
-                    <span className="font-medium text-lg bg-blue-600 rounded-md">tagline</span>
-
-                </div>
-
+        <div className="min-h-screen w-full flex justify-center bg-cover bg-[url('/garen-background.webp')] relative">
+            <div className="content max-w-4xl absolute top-1/3">
+                <h1 className="text-5xl font-bold text-gray-200 text-center mb-16">SummonerStats</h1>
                 <InputForm></InputForm>
-
             </div>
         </div>
     )
--- src/components/UserData.jsx
+++ src/components/UserData.jsx
@@ -6,6 +6,7 @@ import MatchesList from './MatchesList';
 function UserData() {
     const {id} = useParams()
     const [PUUID, setPUUID] = useState(null)
+    const [profileIcon, setProfileIcon] = useState(null)
     const [matchData, setMatchData] = useState(null)
 
     const args = id.split("-")
@@ -24,19 +25,22 @@ function UserData() {
         
     }, [riot_id, tagline]); // Run when `riot_id` or `tagline` changes
 
-    useEffect(()=> {
-      if(PUUID) {
-        // console.log("PUUID set " + PUUID)
-      }  
-
+    useEffect(()=> { 
       const getMatchData = async () => {
         let response = await fetch(`http://127.0.0.1:8000/matches?puuid=${PUUID}&num_matches=2&region=${region}`)
         let data = await response.json()
         setMatchData(data)
       }
 
+      const getProfileIcon = async () => {
+        let response = await fetch(`http://127.0.0.1:8000/profileIcon?puuid=${PUUID}`)
+        let data = await response.json()
+        setProfileIcon(data)
+      }
+
       if(PUUID) {
         getMatchData();
+        getProfileIcon();
       } 
 
     }, [PUUID])
@@ -44,7 +48,12 @@ function UserData() {
     return (
         <div>
             <h2 className="text-white text-3xl font-bold">{riot_id}#{tagline}</h2>
-            <p className="text-white">Region: {region}</p>
+            <div className="flex pb-3 align-bottom">
+              <img src={profileIcon} alt="" className="size-24 rounded-full border border-white" />
+              <p className="text-white">Region: {region}</p>
+            </div>
+            
+
             <MatchesList matchData={matchData} region={region} />
         </div>
     )
