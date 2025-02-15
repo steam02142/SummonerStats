0de0c386fda67f05133178f1871b467e9de945c3
spencer muise <spencermuise@shaw.ca>
2025-02-14 16:33:04 -0800

    Cleaned up conditional rendering for scoreboard

--- src/components/extraMatchData/ExtraMatchData.jsx
+++ src/components/extraMatchData/ExtraMatchData.jsx
@@ -6,28 +6,26 @@ import GoldChart from "./LineChartTest";
 const ExtraMatchData = ({primaryPlayer, blueTeam, redTeam, backgroundColours, matchid, region}) => {
     const [selected, setSelected] = useState("Scoreboard")
 
-
-    if (selected == "Scoreboard") {
-        return (  
-            <div className="bg-[#1B2335]">
-                <InformationSelector selected={selected} setSelected={setSelected}></InformationSelector>
+    return (  
+        <div className="bg-[#1B2335]">
+            <InformationSelector selected={selected} setSelected={setSelected}></InformationSelector>
+            {selected === "Scoreboard" && 
+                <>
                 <ExtraMatchDataRow primaryPlayer={primaryPlayer} players={blueTeam} team="Blue" backgroundColours={backgroundColours}></ExtraMatchDataRow>
                 <ExtraMatchDataRow primaryPlayer={primaryPlayer} players={redTeam} team="Red" backgroundColours={backgroundColours}></ExtraMatchDataRow>
-            </div>  
-        );
-    } else {
-        return (
-            <div className="bg-[#1B2335]">
-                <InformationSelector selected={selected} setSelected={setSelected}></InformationSelector>
+                </>
+            }
+
+            {selected === "Graphs" &&
                 <div 
-                    className={`${primaryPlayer.won ? backgroundColours.win : backgroundColours.loss} 
-                                py-2 flex justify-center`}>
-                    <GoldChart matchid={matchid} region={region} primaryPlayer={primaryPlayer.championName}></GoldChart>
-                </div>
-                
+                className={`${primaryPlayer.won ? backgroundColours.win : backgroundColours.loss} 
+                            py-2 flex justify-center`}>
+                <GoldChart matchid={matchid} region={region} primaryPlayer={primaryPlayer.championName}></GoldChart>
             </div>
-        );
-    }
+            }
+        </div>  
+    );
+    
     
 }
  
